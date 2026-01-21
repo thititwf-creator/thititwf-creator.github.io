3a02367f2e28f0846a13289c78b52482a42c917b
thititwf-creator <thititwf@gmail.com>
2026-01-21 15:26:21 +0700

    add

--- Demo1/js/config.js
+++ Demo1/js/config.js
@@ -1,1 +1,1 @@
-const API = "https://script.google.com/macros/s/AKfycbzPfFVYkIJJ7NLnaNQD2DeidBE15DiXzrb3ikSdMZ9jzO_JAHgFvLDz53AY-cP9igE5/exec";
+const API = "https://script.google.com/macros/s/AKfycbzGHNMtBSIhJfjk6WupY6IqP9V4QSGM4eTfOMKIOkqloOL0Pvn4ZF41FhWZVvpPHExs/exec";
